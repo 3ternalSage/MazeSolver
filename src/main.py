c67af924eda6db4322784d09e1176157e3bfe073
Aaditya Timalsina <aadityatimal@gmail.com>
2024-06-28 21:43:19 -0700

    draw cells

--- src/main.py
+++ src/main.py
@@ -2,13 +2,19 @@ import sys
 sys.path.append('.')
 
 from src.window import Window
-from src.shapes import Line, Point
+from src.shapes import Line, Point, Cell
 
 def main() -> None:
     win: Window = Window(800, 600)
-    l: Line = Line(Point(200, 200), Point(600, 200))
+
+    for i in range(16):
+        c = Cell(10+(i*30)+((i-1)*10), 50, 10+((i+1)*30)+((i-1)*10), 100, win)
+        c.has_left_wall = (i & 0b0001)
+        c.has_right_wall = ((i & 0b0010) >> 1)
+        c.has_top_wall = ((i & 0b0100) >> 2)
+        c.has_bottom_wall = ((i & 0b1000) >> 3)
+        c.draw()
     
-    win.draw_line(l, 'red')
     win.wait_for_close()
 
 if __name__ == '__main__':
--- src/shapes.py
+++ src/shapes.py
@@ -1,6 +1,5 @@
 from tkinter import Canvas
 
-
 class Point:
     def __init__(self, x: int, y: int) -> None:
         self.x: int = x
@@ -13,3 +12,25 @@ class Line:
     
     def draw(self, canvas: Canvas, fill_color: str) -> None:
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, x1: int, y1: int, x2: int, y2: int, window: 'Window') -> None:
+        self.has_left_wall: bool = True
+        self.has_right_wall: bool = True
+        self.has_top_wall: bool = True
+        self.has_bottom_wall: bool = True
+        self._x1: int = x1
+        self._y1: int = y1
+        self._x2: int = x2
+        self._y2: int = y2
+        self._win: 'Window' = window
+
+    def draw(self) -> None:
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), 'black')
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), 'black')
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), 'black')
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), 'black')
